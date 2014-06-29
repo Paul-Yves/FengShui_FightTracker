8dccbfdbc5aa4986dd50d787487454c38061ff04
Paul-Yves <lucas.paulyves@gmail.com>
2014-06-29 18:28:15 +0100

    dialog to create char partly done

--- src/jdr/fengshui/FengShui_FightTracker/activity/Tracker.java
+++ src/jdr/fengshui/FengShui_FightTracker/activity/Tracker.java
@@ -1,10 +1,29 @@
 package jdr.fengshui.FengShui_FightTracker.activity;
 
 import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import jdr.fengshui.FengShui_FightTracker.R;
+import jdr.fengshui.FengShui_FightTracker.fragments.CreateNamedDialog;
+import jdr.fengshui.FengShui_FightTracker.models.*;
+import jdr.fengshui.FengShui_FightTracker.models.Character;
+import jdr.fengshui.FengShui_FightTracker.utils.CharacterAdapter;
+
+import java.util.ArrayList;
 
 public class Tracker extends Activity {
+    private ArrayList<Character> charList;
+    private CharacterAdapter<Character> charAdapter;
+    private Button reinitSeg;
+    private Button addNamed;
+    private Button addMook;
+
+    private final int ADDANAMED = 2;
+    private final int ADDAMOOK = 3;
     /**
      * Called when the activity is first created.
      */
@@ -12,5 +31,34 @@ public class Tracker extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        reinitSeg = (Button) findViewById(R.id.init_button);
+        addNamed = (Button) findViewById(R.id.add_named);
+        addNamed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogType(ADDANAMED);
+            }
+        });
+        addMook = (Button) findViewById(R.id.add_mook);
+
+    }
+
+    public void showDialogType(int type){
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        DialogFragment dial = null;
+        switch (type){
+            case ADDANAMED :
+                dial = CreateNamedDialog.newInstance(R.string.create_newNamed);
+                break;
+            case ADDAMOOK :
+                break;
+        }
+        dial.show(getFragmentManager(),"dialog");
     }
 }
--- src/jdr/fengshui/FengShui_FightTracker/fragments/CreateNamedDialog.java
+++ src/jdr/fengshui/FengShui_FightTracker/fragments/CreateNamedDialog.java
@@ -1,17 +1,83 @@
 package jdr.fengshui.FengShui_FightTracker.fragments;
 
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import jdr.fengshui.FengShui_FightTracker.R;
+import jdr.fengshui.FengShui_FightTracker.models.Named;
 
 /**
  * Created by paulyves on 6/29/14.
  */
-public class CreateNamedDialog extends Fragment {
+public class CreateNamedDialog extends DialogFragment {
+    private EditText name;
+    private EditText mVA;
+    private EditText sVA;
+    private EditText speed;
+    private EditText toughness;
+
+
+    public static CreateNamedDialog newInstance(int title){
+        CreateNamedDialog dialog = new CreateNamedDialog();
+        Bundle args = new Bundle();
+        args.putInt("title", title);
+        dialog.setArguments(args);
+        return dialog;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        View v = inflater.inflate(R.layout.create_named_dialog, container, false);
+
+        Button cancel = (Button) v.findViewById(R.id.cancel);
+        Button validate = (Button) v.findViewById(R.id.validate);
+
+        name = (EditText) v.findViewById(R.id.name);
+        mVA = (EditText) v.findViewById(R.id.mVA);
+        sVA = (EditText) v.findViewById(R.id.sVA);
+        speed = (EditText) v.findViewById(R.id.speed);
+        toughness = (EditText) v.findViewById(R.id.tougness);
+
+        validate.setOnClickListener(validateListener);
+        cancel.setOnClickListener(cancelListener);
+
+        return v;
     }
+    private View.OnClickListener validateListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String charName = name.getText().toString();
+            long result = -1;
+            if(!charName.isEmpty()){
+                try{
+                    int mainVA = Integer.parseInt(mVA.getText().toString());
+                    int secVA = Integer.parseInt(sVA.getText().toString());
+                    int speedVal = Integer.parseInt(speed.getText().toString());
+                    int toughnessVal = Integer.parseInt(toughness.getText().toString());
+                    Named newChar = new Named(charName,mainVA,secVA,speedVal,toughnessVal);
+                    result = 1;
+                    Toast.makeText(getActivity(), newChar.toString(), Toast.LENGTH_SHORT).show();
+                } catch(NumberFormatException e) {
+                    result = -1;
+                }
+            }
+            if(result == -1){
+                Toast.makeText(getActivity(), getText(R.string.errorCreate), Toast.LENGTH_SHORT).show();
+            }
+            dismiss();
+        }
+    };
+
+    private View.OnClickListener cancelListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dismiss();
+        }
+    };
 }
